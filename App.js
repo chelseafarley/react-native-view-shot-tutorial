95ae65ec37c91d9a530c0cc7e8f72c0f26a988e9
Chelsea <chelsea@tripwiretech.com>
2023-02-11 21:22:52 +1300

    tutorial

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { captureRef } from 'react-native-view-shot';
+import { useRef, useState } from 'react';
+
+// npx expo install react-native-view-shot
 
 export default function App() {
+  const viewToSnapshotRef = useRef();
+  const [snapshotImg, setSnapshotImg] = useState();
+
+  const snapshot = async () => {
+    const result = await captureRef(viewToSnapshotRef, { result: 'data-uri' });
+    console.log(result);
+    setSnapshotImg(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View ref={viewToSnapshotRef} style={styles.snapshot}>
+        <Text>Contact Details</Text>
+        <Text>Names: James Smith</Text>
+        <Text>Phone: 555-5555</Text>
+      </View>
+      <Button title="Take Snapshot" onPress={snapshot} />
+      {snapshotImg && <Text>Snapshot Preview:</Text>}
+      {snapshotImg && <Image resizeMode="contain" style={styles.snapshotImg} source={{ uri: snapshotImg}} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +37,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  snapshot: {
+    borderColor: "#000000",
+    borderWidth: 2,
+    backgroundColor: "#00ffff",
+    margin: 16,
+    padding: 16
+  },
+  snapshotImg: {
+    width: 400,
+    height: 200,
+    margin: 16
+  }
 });
